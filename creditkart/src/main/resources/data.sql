077a051face4f5e842b0c7cf9f201b9387e618ab
Upasana Ghosh <upasana.ghosh@stonewain.com>
2020-01-23 01:19:26 +0530

    Data file updated

--- creditkart/src/main/resources/data.sql
+++ creditkart/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into t_customer (customer_id, name, address, username, password, is_admin)
-values (1, 'Amit', 'Delhi', 'amit', 'amit@123', true);
+values (101, 'Amit', 'Delhi', 'amit', 'amit@123', true);
 
 insert into t_customer (customer_id, name, address, username, password, is_admin)
-values (2, 'Rohit', 'Mumbai', 'rohit', 'rohit@123', false);
+values (201, 'Rohit', 'Mumbai', 'rohit', 'rohit@123', false);
 
 insert into t_account (account_id, customer_id, bank_name, balance, credit)
 values (101, 1, 'HDFC', 1000, 230);
